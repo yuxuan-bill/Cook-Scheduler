bb3271be3cece4c69bc5edad2effa36504a5a9bf
yuxuan-bill <luyxapp@163.com>
2019-12-21 15:33:26 -0800

    improve model, write test

--- cookschedule/models.py
+++ cookschedule/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from datetime import timedelta
-from datetime import datetime
+from datetime import date, datetime
 from typing import List, Dict
 
 
 # Create your models here.
 
-
-participants = ["Bill", "Daniel", "Darcy", "Leo"]
 breakfast = timedelta(hours=8, minutes=30)
 lunch = timedelta(hours=12, minutes=30)
 dinner = timedelta(hours=18, minutes=30)
@@ -19,7 +17,7 @@ dinner = timedelta(hours=18, minutes=30)
 def score() -> Dict[str, float]:
     schedules = history()
     scores = {}
-    for participant in participants:
+    for participant, _ in Schedule.participants:
         scores[participant] = 0.0
     for schedule in schedules:
         eaters = schedule.eaters()
@@ -33,22 +31,15 @@ def score() -> Dict[str, float]:
 
 # add to schedule if the date + meal combination is new, otherwise
 # modify existing entry. Return True on success, False on failure.
-def add(date: datetime, meal: timedelta,
+def add(day: date, meal: timedelta,
         cooks: List[str], eaters: List[str], note, cancelled=False):
     if meal not in [breakfast, lunch, dinner]:
         return False
-    for cook in cooks:
-        if cook not in participants:
-            return False
-    for eater in eaters:
-        if eater not in participants:
-            return False
-    time = date + meal
+    time = day + meal
     try:
         schedule = Schedule.objects.get(time=time)
     except Schedule.DoesNotExist:
-        schedule = Schedule(time=time, update_time=datetime.now(),
-                            cancelled=cancelled, note=note)
+        schedule = Schedule(time=time, cancelled=cancelled, note=note)
         schedule.save()
         schedule.set_cooks(cooks)
         schedule.set_eaters(eaters)
@@ -57,7 +48,6 @@ def add(date: datetime, meal: timedelta,
         schedule.time = time
         schedule.note = note
         schedule.cancelled = cancelled
-        schedule.update_time = datetime.now()
         schedule.set_cooks(cooks)
         schedule.set_eaters(eaters)
         return True
@@ -66,17 +56,16 @@ def add(date: datetime, meal: timedelta,
 # cancel the schedule specified by date and meal, return False if
 # meal is not one of breakfast/lunch/dinner or the entry does not
 # exist, otherwise True
-def delete(date, meal):
+def delete(day: date, meal: timedelta):
     if meal not in [breakfast, lunch, dinner]:
         return False
-    time = date + meal
+    time = day + meal
     try:
         schedule = Schedule.objects.get(time=time)
     except Schedule.DoesNotExist:
         return False
     else:
         schedule.cancelled = True
-        schedule.update_time = datetime.now()
         schedule.save()
         return True
 
@@ -95,37 +84,44 @@ def plan() -> List['Schedule']:
 
 class Schedule(models.Model):
 
+    bill = "Bill"
+    daniel = "Daniel"
+    darcy = "Darcy"
+    leo = "Leo"
+    participants = [(bill, "Bill"), (daniel, "Daniel"),
+                    (darcy, "Darcy"), (leo, "Leo")]
+
     # have time value 8:30, 12:30 or 18:30, denoting the time when a
     # planned schedule should become history. Can be formed using
     # datetime(year, month, day) + breakfast/lunch/dinner
     time = models.DateTimeField(primary_key=True)
-    update_time = models.DateTimeField()
-    cancelled = models.BinaryField(default=False)
+    update_time = models.DateTimeField(auto_now=True)
+    cancelled = models.BooleanField(default=False)
     note = models.CharField(max_length=1024)
 
     def __str__(self):
         return str(self.getinfo())
 
-    def cooks(self):
+    def cooks(self) -> List[str]:
         names = []
         for cook in self.cook_set.all():
             names.append(cook.name)
         return names
 
-    def set_cooks(self, cooks):
+    def set_cooks(self, cooks: List[str]):
         self.cook_set.all().delete()
         for cook in cooks:
             self.cook_set.create(time=self.time, name=cook).save()
         self.update_time = datetime.now()
         self.save()
 
-    def eaters(self):
+    def eaters(self) -> List[str]:
         names = []
         for eater in self.eat_set.all():
             names.append(eater.name)
         return names
 
-    def set_eaters(self, eaters):
+    def set_eaters(self, eaters: List[str]):
         self.eat_set.all().delete()
         for eater in eaters:
             self.eat_set.create(time=self.time, name=eater).save()
@@ -144,10 +140,10 @@ class Schedule(models.Model):
 class Cook(models.Model):
 
     time = models.ForeignKey(Schedule, on_delete=models.CASCADE)
-    name = models.CharField(max_length=16)
+    name = models.CharField(choices=Schedule.participants, max_length=16)
 
 
 class Eat(models.Model):
 
     time = models.ForeignKey(Schedule, on_delete=models.CASCADE)
-    name = models.CharField(max_length=16)
+    name = models.CharField(choices=Schedule.participants, max_length=16)
--- cookschedule/tests.py
+++ cookschedule/tests.py
@@ -1,3 +1,107 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+leo = Schedule.leo
+darcy = Schedule.darcy
+bill = Schedule.bill
+daniel = Schedule.daniel
+participants = [x for x, _ in Schedule.participants]
+
+
+class ScheduleModelTests(TestCase):
+
+    def setUp(self):
+
+        self.today = datetime.now().date()
+
+        future_schedule = Schedule.objects.create(
+            time=self.today + timedelta(days=1) + breakfast,
+            note="future schedule"
+        )
+        future_schedule.set_eaters([leo, darcy, bill])
+        future_schedule.set_cooks([leo, darcy])
+
+        past_schedule1 = Schedule.objects.create(
+            time=self.today + timedelta(days=-1) + lunch,
+            note="lunch for yesterday"
+        )
+        past_schedule1.set_eaters([leo, darcy, bill])
+        past_schedule1.set_cooks([leo, darcy])
+
+        past_schedule2 = Schedule.objects.create(
+            time=self.today + timedelta(days=-1) + dinner,
+            note="dinner@for#yesterday!"
+        )
+        past_schedule2.set_cooks([bill])
+        past_schedule2.set_eaters(participants)
+
+        cancelled_schedule = Schedule.objects.create(
+            time=self.today + lunch,
+            update_time=datetime.now(),
+            cancelled=True,
+            note="cancelled schedule"
+        )
+        cancelled_schedule.set_eaters(participants)
+        cancelled_schedule.set_cooks(participants)
+
+    def test_schema(self):
+        schedules = Schedule.objects.all()
+        self.assertEquals(len(schedules), 3)
+        info2 = {'time': self.today + timedelta(days=1) + breakfast,
+                 'cancelled': False,
+                 'cooks': [bill],
+                 'eaters': [bill, daniel, darcy, leo],
+                 'note': "dinner@for#yesterday!"}
+        actual_info = schedules[2].getinfo()
+        self.assertLessEqual(
+            abs((actual_info['update_time'] - datetime.now()).total_seconds()),
+            5)
+        del actual_info['update_time']
+        self.assertEquals(actual_info, info2)
+
+        future_schedule = Schedule.objects.get(
+            time=self.today + timedelta(days=1) + breakfast)
+        self.assertEquals(future_schedule.note, "future schedule")
+
+    def test_add_new_schedule(self):
+        day = datetime(year=2000, month=1, day=1).date()
+        add(day=day,
+            meal=lunch,
+            cooks=[darcy],
+            eaters=[darcy, leo],
+            note='new schedule')
+        schedule = Schedule.objects.get(time=day + lunch)
+        self.assertEquals(schedule.cooks(), [darcy])
+        self.assertEquals(schedule.note, 'new schedule')
+
+    def test_modify_existent_schedule(self):
+        add(day=self.today,
+            meal=lunch,
+            cooks=[darcy, bill, leo],
+            eaters=participants,
+            note='restore schedule')
+        schedule = Schedule.objects.get(time=self.today + lunch)
+        self.assertEquals(schedule.cancelled, False)
+        self.assertEquals(schedule.cooks(), [darcy, bill, leo])
+
+    def test_delete_schedule(self):
+        delete(self.today + timedelta(days=1), breakfast)
+        self.assertEquals(len(plan()), 0)
+
+    def test_get_score(self):
+        scores = score()
+        self.assertEquals(scores,
+                          {bill: 2, darcy: -0.5, leo: -0.5, daniel: -1})
+
+    def test_get_history_schedule(self):
+        history_schedule = history()
+        self.assertEquals(len(history_schedule), 2)
+        self.assertEquals(history_schedule[0].note, "lunch for yesterday")
+        self.assertEquals(history_schedule[1].note, "dinner@for#yesterday")
+
+    def test_get_planned_schedule(self):
+        planned_schedule = plan()
+        self.assertEquals(len(planned_schedule), 1)
+        self.assertEquals(planned_schedule[0].note, "future schedule")
