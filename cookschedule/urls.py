b9b2a79dab288373791efea391288b23e4a43f86
yuxuan-bill <luyxapp@163.com>
2019-12-22 16:09:00 -0800

    finish index template, prepare to link to view (frontend sucks)

--- cookschedule/urls.py
+++ cookschedule/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.urls import path
+from . import views
 
+app_name = 'cookschedule'
+
+urlpatterns = [
+    path('', views.index, name='index')
 ]
--- cookschedule/views.py
+++ cookschedule/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+
+
+def index(request):
+    context = {'scores': score(),
+               'plan': process_schedules(plan()),
+               'history': process_schedules(history()),
+               'today': str(datetime.today().date())
+               }
+    if request.POST:
+        print(request.POST)
+    return render(request, 'cookschedule/index.html', context)
+
+
+def process_schedules(schedules: List['Schedule']):
+    result = []
+    for schedule in schedules:
+        meal_time = schedule.time.time()
+        meal_delta = timedelta(hours=meal_time.hour, minutes=meal_time.minute)
+        result.append({'cooks': ", ".join(schedule.cooks()),
+                       'eaters': ", ".join(schedule.eaters()),
+                       'notes': schedule.note,
+                       'date': str(schedule.time.date()),
+                       'meal': meal_dict[meal_delta],
+                       'update_time': str(schedule.update_time)
+                       })
+    return result
