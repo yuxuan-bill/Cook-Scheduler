d5472d766bc4372b46f21755852e8131c51a72db
yuxuan-bill <awesomel@uw.edu>
2019-12-29 19:40:09 -0800

    add production settings

--- vita/settings.py
+++ vita/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = credential.secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['10.154.126.49']
+ALLOWED_HOSTS = ['10.154.127.166', '192.168.0.125']
 
 
 # Application definition
